46f46eed64540ad56b1e42aefef215df601bc5d2
Johan Litaudon <jlitaudo@z3r8p3.42lyon.fr>
2023-04-12 15:20:13 +0200

    last push

--- CPP-04/ex01/main.cpp
+++ CPP-04/ex01/main.cpp
@@ -9,37 +9,41 @@
 
 int main(void)
 {
-	{
-		Animal *animals[10];
-		for (int i = 0; i < 10; i++) {
-			if (i % 2) {
-				animals[i] = new Dog();
-			} else {
-				animals[i] = new Cat();
-			}
-		}
-		for (int i = 0; i < 10; i++) {
-			delete animals[i];
-		}
-	}
-	{
-		Animal *animals[2];
-		animals[0] = new Cat();
-		animals[1] = new Dog();
+//	{
+//		Animal *animals[10];
+//		for (int i = 0; i < 10; i++) {
+//			if (i % 2) {
+//				animals[i] = new Dog();
+//			} else {
+//				animals[i] = new Cat();
+//			}
+//		}
+//		for (int i = 0; i < 10; i++) {
+//			delete animals[i];
+//		}
+//	}
+//	{
+//		Animal *animals[2];
+//		animals[0] = new Cat();
+//		animals[1] = new Dog();
+//
+//		Brain *brain = animals[1]->getBrain();
+//		brain->ideas[0] = "test";
+//		brain->ideas[1] = "idea";
+//		std::cout << "-------- Assigment operator --------" << std::endl;
+//		*(animals[0]) = *(animals[1]);
+//		std::cout << brain->ideas[0] << std::endl;
+//		std::cout << "-------- Brain content --------" << std::endl;
+//		std::cout << animals[0]->getBrain()->ideas[0] << std::endl;
+//		std::cout << animals[0]->getBrain()->ideas[1] << std::endl;
+//		std::cout << "-------- Destructors --------" << std::endl;
+//		delete animals[0];
+//		delete animals[1];
 
-		Brain *brain = animals[1]->getBrain();
-		brain->ideas[0] = "test";
-		brain->ideas[1] = "idea";
-		std::cout << "-------- Assigment operator --------" << std::endl;
-		*(animals[0]) = *(animals[1]);
-		std::cout << brain->ideas[0] << std::endl;
-		std::cout << "-------- Brain content --------" << std::endl;
-		std::cout << animals[0]->getBrain()->ideas[0] << std::endl;
-		std::cout << animals[0]->getBrain()->ideas[1] << std::endl;
-		std::cout << "-------- Destructors --------" << std::endl;
-		delete animals[0];
-		delete animals[1];
+	Dog Basic;
+	{
+		Dog tmp = Basic;
 	}
-
+		std::cout << std::endl;
 	return 0;
 }
